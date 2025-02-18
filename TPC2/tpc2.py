6ec7cab2a8df1a342a76efbf7cd6ee9625ba9be1
Afonsosr <113983116+Afonsosr@users.noreply.github.com>
2025-02-18 16:45:17 +0000

    Update tpc2.py

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -83,7 +83,7 @@ def soma_string(s):
 
 # 6)
 
-def inteiros(s):
+def pronomes(s):
     lista = re.split(r' ',s)
     final = []
     for elem in lista:
@@ -107,8 +107,9 @@ def variavel_valida(s):
 
 # 8)
 
-def inteiros(lista):
+def inteiros(s):
     final = []
+    lista = re.split(r' ',s)
     for elem in lista:
         a = re.fullmatch(r'-?\d+',elem)
         if a != None:
@@ -152,12 +153,12 @@ print(palavra_magica("Preciso de um favor."))
 narcisismo("Eu não sei se eu quero continuar a ser eu. Por outro lado, eu ser eu é uma parte importante de quem EU sou.")
 troca_de_curso("LEI é o melhor curso! Adoro LEI! Gostar de LEI devia ser uma lei.", "Biomédica")
 soma_string("4,-6,2,3,8,-3,0,2,-5,1")
-inteiros("eu tu nos vozes voces vos voz")
-inteiros("amanha vamos ao cinema, eu e eles")    
+pronomes("eu tu nos vozes voces vos voz")
+pronomes("amanha vamos ao cinema, eu e eles")    
 variavel_valida("Olá")
 variavel_valida("2+3=5")
 variavel_valida("1pessoa")
 variavel_valida("p_e_s_s_o_a_")
-inteiros(["2","2","6","1,4","112.6543","-4","a","-432,2","w23","23d","8","12"])
+inteiros("vou comprar 3 pecas de fruta, e 3.3 euros em doces e 2,4 euros em macas")
 underscores("Eu    não sei se   eu quero continuar .")
 codigo_postal(["4700-000","1234-567","8541-543","4123-974","9481-025"])
